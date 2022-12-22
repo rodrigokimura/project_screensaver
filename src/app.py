5c07170b0991d694ec8701ca604b4f88b4a94519
Rodrigo Eiti Kimura <rodrigo.kimura@catskillet.com>
2022-12-21 21:14:12 -0300

    Add layouts classes

--- src/app.py
+++ src/app.py
@@ -1,82 +1,37 @@
-import sys
-from tkinter import Frame, Tk, Toplevel
+from tkinter import Tk, Toplevel
 from typing import List, Union
 
+from layouts import Layout, SolidColor
 from utils import get_display_sizes_and_position
 
 
 class Screensaver:
 
-    windows: List[Union[Tk, Toplevel]]
-    state: bool
+    displays: List[Union[Tk, Toplevel]]
+    layouts: List[Layout]
 
-    def __init__(self):
-        self.state = False
-        self.render()
-        self.bind_keys()
-        self.hide_cursor()
-        self.toggle_fullscreen()
+    def __init__(self, layouts: List[Layout]):
+        self.layouts = layouts
+        self.configure()
 
-    def bind_keys(self):
-        for window in self.windows:
-            window.bind("<Escape>", self.exit)
-
-    def render(self):
-        displays = get_display_sizes_and_position()
-        main_monitor = displays.pop()
-
+    def configure(self):
         self.tk = Tk()
-        self.tk.attributes("-zoomed", True)
-        self.tk.geometry(
-            f"{main_monitor.width}x{main_monitor.height}+{main_monitor.x}+{main_monitor.y}"
-        )
+        self.tk.withdraw()
 
-        self.frame = Frame(self.tk)
-        self.frame.pack()
+        displays = get_display_sizes_and_position()
 
-        self.windows = [self.tk]
-
-        for display in displays:
-            new_window = self.render_extra_monitor(
-                (display.width, display.height), (display.x, display.y)
+        for i, display in enumerate(displays):
+            layout = self.layouts[i]
+            layout.configure(
+                self.tk, (display.width, display.height), (display.x, display.y)
             )
-            self.windows.append(new_window)
-        
-        for window in self.windows:
-            window.configure(bg='black')
-
-    def render_extra_monitor(self, size, position):
-        new_window = Toplevel(self.tk)
-        new_window.title("New Window")
-        w, h = size
-        x, y = position
-        new_window.geometry(f"{w}x{h}+{x}+{y}")
-        return new_window
-
-    def hide_cursor(self):
-        for window in self.windows:
-            window.config(cursor="none")
-
-    def toggle_fullscreen(self, event=None):
-        self.state = not self.state
-        for window in self.windows:
-            window.attributes("-fullscreen", self.state)
-        return "break"
-
-    def end_fullscreen(self, event=None):
-        self.state = False
-        for window in self.windows:
-            window.attributes("-fullscreen", False)
-        return "break"
-
-    def exit(self, event=None):
-        self.tk.destroy()
-        sys.exit()
 
     def start(self):
         self.tk.mainloop()
 
 
 if __name__ == "__main__":
-    w = Screensaver()
+    w = Screensaver(
+        layouts=[SolidColor("black"), SolidColor("black"), SolidColor("black")]
+    )
     w.start()
